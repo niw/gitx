6d998711f875179d21a38f6fc695da90962d8aac
Yoshimasa Niwa <niw@niw.at>
2012-01-24 14:10:34 -0800

    FIX: no ppc, any more but x86_64.

--- build_libgit2.sh
+++ build_libgit2.sh
@@ -9,25 +9,11 @@
 # based on: http://log.yeahrightkeller.com/post/270155578/run-script-while-cleaning-in-xcode
 
 buildAction () {
-    echo "Building libgit2..."
-	if [[ -d .git ]]
-	then
-        if [ "$(which git)" == "" ]; then
-            echo "git not found"
-            exit -1
-        fi
-		git submodule init
-		git submodule sync
-		git submodule update
-		cd libgit2
-		rm -f libgit2.a
-		make CFLAGS="-arch i386 -arch ppc"
-		ranlib libgit2.a
-	else
-		echo "error: Not a git repository."
-		echo "error: clone GitX first so that the libgit2 submodule can be updated"
-		exit 1
-	fi
+	echo "Building libgit2..."
+	cd libgit2
+	rm -f libgit2.a
+	make CFLAGS="-arch i386 -arch x86_64"
+	ranlib libgit2.a
 }
 
 cleanAction () {
@@ -36,21 +22,14 @@ cleanAction () {
 	make clean
 }
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# MAIN
-
-#echo "Running with ACTION=${ACTION}"
-
 case $ACTION in
 	# NOTE: it gets set to "" rather than "build" when doing a build.
 	"")
 		buildAction
 		;;
-
 	"clean")
 		cleanAction
 		;;
 esac
 
 exit 0
-
