7b58bdd54ceca663d836c6e8c36c8bad1df7d0db
Yoshimasa Niwa <niw@niw.at>
2012-01-25 01:07:39 -0800

    Clean up build script.

--- build_libgit2.sh
+++ build_libgit2.sh
@@ -9,21 +9,11 @@
 # based on: http://log.yeahrightkeller.com/post/270155578/run-script-while-cleaning-in-xcode
 
 buildAction () {
-    echo "Building libgit2..."
-	if [[ -d .git ]]
-	then
-		export PATH=$PATH:$HOME/bin:$HOME/local/bin:/sw/bin:/opt/local/bin:`"$TARGET_BUILD_DIR"/gitx --git-path`
-		git submodule init
-		git submodule update
-		cd libgit2
-		rm -f libgit2.a
-		make CFLAGS="-arch i386 -arch x86_64"
-		ranlib libgit2.a
-	else
-		echo "error: Not a git repository."
-		echo "error: clone GitX first so that the libgit2 submodule can be updated"
-		exit 1
-	fi
+	echo "Building libgit2..."
+	cd libgit2
+	rm -f libgit2.a
+	make CFLAGS="-arch i386 -arch x86_64"
+	ranlib libgit2.a
 }
 
 cleanAction () {
@@ -32,21 +22,14 @@ cleanAction () {
 	make clean
 }
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# MAIN
-
-#echo "Running with ACTION=${ACTION}"
-
 case $ACTION in
 	# NOTE: it gets set to "" rather than "build" when doing a build.
 	"")
 		buildAction
 		;;
-
 	"clean")
 		cleanAction
 		;;
 esac
 
 exit 0
-
